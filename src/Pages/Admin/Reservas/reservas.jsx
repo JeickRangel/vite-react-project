27af948123642509a4deddea7443330280504190
JeickRangel <jeisson_rangel@soy.sena.edu.co>
2025-10-28 20:54:07 -0500

    Fix en reservas por case sensitive

--- src/Pages/Admin/Reservas/reservas.jsx
+++ src/Pages/Admin/Reservas/reservas.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "./Reservas.module.css";
+import styles from "./reservas.module.css";
 
 export default function Reservas() {
   const [reservas, setReservas] = useState([]);
